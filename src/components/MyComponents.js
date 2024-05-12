782a7e5176f45cd16f8216e3cc0a0428ea2ea753
tempo1406 <134191285+tempo1406@users.noreply.github.com>
2024-05-12 22:22:15 +0700

    init project

---
+++ src/components/MyComponents.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponents extends React.Component {
+  // JSX
+  render() {
+    return(
+        <div> my first Component
+            { Math.random()}
+        </div>
+        ); 
+  }
+}
+
+export default MyComponents;
