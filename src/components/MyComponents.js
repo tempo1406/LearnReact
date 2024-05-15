864d0202360cbc37ff587ef53ca0feed96b638be
tempo1406 <134191285+tempo1406@users.noreply.github.com>
2024-05-15 23:48:52 +0700

    Dom Events

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -7,11 +7,21 @@ class MyComponents extends React.Component {
     age: 19,
   };
 
+  handleClick(event) {
+    console.log("click button");
+    console.log("My name is ", this.state.name);
+  }
+
+  handleOnMoverOver(event) {
+    console.log(event);
+  }
   // JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and I'm from {this.state.address}
+        <button onClick={this.handleClick}>click me</button>
+        <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
       </div>
     );
   }
