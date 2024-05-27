17702ad44dcc401412f15fa7feb48bfbf93fe595
tempo1406 <134191285+tempo1406@users.noreply.github.com>
2024-05-27 20:49:26 +0700

    img

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './DisplayInfor.scss'
+import logo from './../logo.svg'
 class DisplayInfor extends React.Component {
 
     state = {
@@ -15,6 +16,7 @@ class DisplayInfor extends React.Component {
         const {listUsers} =  this.props
         return (
             <div className="display-infor-container">
+                <img src={logo}/>
                 <div>
                     <span onClick = {() => { this.handleShowHide()}}> 
                     {this.state.isShowListUser === true ? "Hide list users:" : "Show list users:"}
