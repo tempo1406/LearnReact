2e3dc667acd6adea167a23be400dfbfd68c3a5b9
tempo1406 <134191285+tempo1406@users.noreply.github.com>
2024-05-28 13:21:50 +0700

    React Lifecycle

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -3,15 +3,41 @@ import './DisplayInfor.scss'
 import logo from './../logo.svg'
 class DisplayInfor extends React.Component {
 
-    state = {
-        isShowListUser: true
+    constructor(props) {
+        console.log('call constructor: 1')
+        super(props)
+        this.state = {
+            isShowListUser: true
+        }
     }
+
+    // state = {
+    //     isShowListUser: true
+    // }
+
+    componentDidMount(){
+        console.log('call me compent did mount')
+        setTimeout(() => {
+            document.title = 'React'
+        }, 3000)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log('call me compent did update', this.props, prevProps)
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if (this.props.listUsers.length === 5) {
+                alert('You got 5 users')
+            }
+        }
+    }
+
     handleShowHide = () => {
         this.setState({
             isShowListUser: !this.state.isShowListUser
         })
     }
     render() {
+        console.log('call me render')
         //destructuring array/object
         const {listUsers} =  this.props
         return (
