dc3e26c3286c818cb194ba06a7dcce9c9cd5a5c1
tempo1406 <134191285+tempo1406@users.noreply.github.com>
2024-05-28 14:30:08 +0700

    useEffect

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,34 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss'
 import logo from './../logo.svg'
-// class DisplayInfor extends React.Component {
-//     render() {
-//         //destructuring array/object
-//         const {listUsers} =  this.props
-//         return (
-//             <div className="display-infor-container">
-//                 {true && 
-//                     <>
-//                     { listUsers.map((user) => {
-//                         return (
-//                             <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                                 <div>
-//                                     <div>My name's {user.name}</div>
-//                                     <div>My age {user.age}</div>  
-//                                 </div>
-//                                 <div>
-//                                     <button onClick={() => this.props.hanleDeleteUser(user.id)}>Delete</button>
-//                                 </div>
-//                                 <hr />
-//                             </div>
-//                         ) 
-//                     })}
-//                     </> 
-//                 }           
-//             </div>
-//         ) 
-//     }
-// }
 
 const DisplayInfor = (props) => {
     const {listUsers} = props // object
@@ -38,6 +10,20 @@ const DisplayInfor = (props) => {
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser)
     }
+
+
+
+    useEffect(
+        () => {
+        setTimeout(() => {
+            if(listUsers.length === 0) {
+                alert('ddd')
+            }
+            document.title = 'Nguyen Ngoc Toan'
+        }, 3000)
+        console.log('call me useEffect')
+        },[listUsers] // [] Chay 1 lan = componentDidMount
+    )
 
         return (
             <div className="display-infor-container">
